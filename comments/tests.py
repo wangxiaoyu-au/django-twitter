e1d3d516c049cae39113375afb1f7d719659642a
Wang Xiaoyu <wangxiaoyu@Yins-MBP.gateway>
2022-03-01 15:07:03 +1100

    Like Model and Admin

--- comments/tests.py
+++ comments/tests.py
@@ -3,8 +3,23 @@ from testing.testcases import TestCase
 
 class CommentModelTests(TestCase):
 
+    def setUp(self):
+        self.pluto = self.create_user('pluto')
+        self.tweet = self.create_tweet(self.pluto)
+        self.comment = self.create_comment(self.pluto, self.tweet)
+
     def test_comment(self):
-        user = self.create_user('pluto')
-        tweet = self.create_tweet(user)
-        comment = self.create_comment(user, tweet)
-        self.assertNotEqual(comment.__str__(), None)
+        self.assertNotEqual(self.comment.__str__(), None)
+
+    def test_like_set(self):
+        self.create_like(self.pluto, self.comment)
+        self.assertEqual(self.comment.like_set.count(), 1)
+
+        # the same user liked the same comment again,
+        # this time the like would be ignored
+        self.create_like(self.pluto, self.comment)
+        self.assertEqual(self.comment.like_set.count(), 1)
+
+        brunch = self.create_user('brunch')
+        self.create_like(brunch, self.comment)
+        self.assertEqual(self.comment.like_set.count(), 2)
--- testing/testcases.py
+++ testing/testcases.py
@@ -2,7 +2,9 @@ from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
 from tweets.models import Tweet
 from comments.models import Comment
+from likes.models import Like
 from rest_framework.test import APIClient
+from django.contrib.contenttypes.models import ContentType
 
 
 class TestCase(DjangoTestCase):
@@ -35,3 +37,12 @@ class TestCase(DjangoTestCase):
             tweet=tweet,
             content=content,
         )
+
+    def create_like(self, user, target):
+        # target is a comment or a tweet
+        instance, _ = Like.objects.get_or_create(
+            content_type=ContentType.objects.get_for_model(target.__class__),
+            object_id=target.id,
+            user=user,
+        )
+        return instance

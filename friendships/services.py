911d9ab2324472695423bf8b68ceaaf30d7344eb
Xiaoyu <wangxiaoyu.au@gmail.com>
2022-06-14 15:16:27 +1000

    add gatekeeper to hbase friendship and update friendship serializer

--- friendships/services.py
+++ friendships/services.py
@@ -2,6 +2,10 @@ from friendships.models import Friendship
 from django.conf import settings
 from django.core.cache import caches
 from twitter.cache import FOLLOWINGS_PATTERN
+from friendships.hbase_models import HBaseFollower, HBaseFollowing
+from gatekeeper.models import GateKeeper
+
+import time
 
 cache = caches['testing'] if settings.TESTING else caches['default']
 
@@ -63,3 +67,42 @@ class FriendshipService(object):
     def invalidate_following_cache(cls, from_user_id):
         key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
         cache.delete(key)
+
+    @classmethod
+    def follow(cls, following_user_id, followed_user_id):
+        if following_user_id == followed_user_id:
+            return None
+
+        if not GateKeeper.is_switch_on('switch_friendship_to_hbase'):
+            # create data in mysql
+            return Friendship.objects.create(
+                following_user_id=following_user_id,
+                followed_user_id=followed_user_id,
+            )
+
+        # create data in hbase
+        now = int(time.time() * 1000000)
+        HBaseFollower.create(
+            following_user_id=following_user_id,
+            followed_user_id=followed_user_id,
+            created_at=now,
+        )
+        return HBaseFollowing.create(
+            following_user_id=following_user_id,
+            followed_user_id=followed_user_id,
+            created_at=now,
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
